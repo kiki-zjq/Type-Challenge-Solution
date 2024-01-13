7b3e98e80acaf7b27b80a087626107b79dc281de
朱江奇 <zhujiangqi0701@gmail.com>
2024-01-12 19:53:19 -0800

    feat

---
+++ medium/2070.DropChar.ts
@@ -0,0 +1,56 @@
+/*
+  2070 - Drop Char
+  -------
+  by CaptainOfPhB (@CaptainOfPhB) #medium #template-literal #infer
+
+  ### Question
+
+  Drop a specified char from a string.
+
+  For example:
+
+  ```ts
+  type Butterfly = DropChar<' b u t t e r f l y ! ', ' '> // 'butterfly!'
+  ```
+
+  > View on GitHub: https://tsch.js.org/2070
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type DropChar<S, C> = 
+  S extends `${infer X}${infer Y}`
+    ? X extends C 
+      ? DropChar<Y, C> 
+      : `${X}${DropChar<Y, C>}`
+    : ''
+
+// type DropChar<S extends string, C extends string> = 
+// 	S extends `${infer X}${C}${infer Y}` ? `${X}${DropChar<Y, C>}`: S
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
+  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2070/answer
+  > View solutions: https://tsch.js.org/2070/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/**
+ * Description:
+ *  这一题让我们将指定的 char 从一个 string 中消除
+ *  我们只需要递归的判断当前的 char 是否需要消除即可 
+ */
